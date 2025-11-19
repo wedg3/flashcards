fd89b2fbfa3514c60bd8f3229722f480c8cc3d45
wedg3 <lindgren@gmail.com>
2025-11-19 17:05:24 +0100

    Update wordlist title for gymnidrott1.js

--- wordlists/gymnidrott1.js
+++ wordlists/gymnidrott1.js
@@ -1,4 +1,4 @@
-const wordlisttitle = "Öva på begrepp om muskulära systemet, syresättning, nervsystemet och energigivande processer";
+const wordlisttitle = "Idrott: Kroppen";
 
 const words = [
   { front: "Hjärtmuskel", back: "Muskeltyp som endast finns i hjärtat. <br><br><b>Arbetar hela livet</b> utan viljestyrning." },
