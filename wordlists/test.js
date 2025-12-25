afcd0a4268439daf5504971054201b5fd7dd9e21
wedg3 <lindgren@gmail.com>
2025-12-25 11:18:47 +0100

    Delete wordlists/test.js

--- wordlists/test.js
+++
@@ -1,9 +0,0 @@
-const wordlisttitle = "Öva på begrepp om Andra världskriget";
-
-const words = [
-  { front: "Andra världskriget", back: "Ett globalt krig som pågick mellan <b>1939–1945</b> och involverade de flesta av världens länder. <br><br>Ett globalt krig som pågick mellan <b>1939–1945</b> och involverade de flesta av världens länder. olverade de flesta av världens länder.olverade de flesta av världens länder. Ett globalt krig som pågick mellan <b>1939–1945</b> <br>och involverade de flesta av världens länder." },
-  { front: "Axelmakterna", back: "Allians mellan <b>Tyskland</b>, <b>Italien</b> och <b>Japan</b> under andra världskriget." },
-  { front: "Hiroshima och Nagasaki", back: "Städer i Japan där <b>USA fällde atombomber</b> i augusti 1945." },
-  { front: "Kapitulering", back: "När ett land <i>ger upp</i> i ett krig. Tyskland kapitulerade den <b>8 maj 1945</b>." },
-  { front: "FN (Förenta Nationerna)", back: "Internationell organisation som bildades <b>1945</b> för att bevara fred och förhindra framtida krig." }
-];
