f124067694366ac1b081e8959de852d2a5c176a2
wedg3 <lindgren@gmail.com>
2026-01-28 08:12:17 +0100

    Add nuclear energy wordlist with definitions

---
+++ wordlists/nuclear1.js
@@ -0,0 +1,60 @@
+const wordlisttitle = "Kärnenergi";
+
+const words = [
+  {
+    front: "Fusion",
+    back: "När <b>två atomkärnor slås ihop</b> till en större.<br><br>Då frigörs <i>mycket energi</i>, som i solen."
+  },
+  {
+    front: "Fission",
+    back: "När <b>en atomkärna delas</b> i två mindre delar.<br><br>Då frigörs <i>energi</i>."
+  },
+  {
+    front: "Kärnreaktion",
+    back: "En reaktion där <b>atomkärnor förändras</b>.<br><br>Det kan vara <i>fission eller fusion</i>."
+  },
+  {
+    front: "Kärnbränsle",
+    back: "Material som används i <b>kärnkraftverk</b>.<br><br>Ofta är det <i>uran</i>."
+  },
+  {
+    front: "Klyvningsprodukter",
+    back: "De <b>mindre delar</b> som bildas när en atomkärna delas.<br><br>De är ofta <i>radioaktiva</i>."
+  },
+  {
+    front: "Kärnavfall",
+    back: "Avfall som blir kvar efter kärnreaktioner.<br><br>Det kan vara <i>farligt</i> under lång tid."
+  },
+  {
+    front: "Moderator",
+    back: "Ett ämne som <b>bromsar neutroner</b> i en reaktor.<br><br>Gör att reaktionen sker <i>lugnt</i>."
+  },
+  {
+    front: "Styrstavar",
+    back: "Stavar som <b>stoppar eller minskar</b> reaktionen.<br><br>De suger upp <i>neutroner</i>."
+  },
+  {
+    front: "Kärnavfall (lågaktivt, medelaktivt, högaktivt)",
+    back: "<b>Lågaktivt:</b> svag strålning.<br><br><b>Medelaktivt:</b> starkare strålning.<br><br><b>Högaktivt:</b> mycket farligt under <i>lång tid</i>."
+  },
+  {
+    front: "Atombomb",
+    back: "Ett vapen som använder <b>okontrollerad kärnreaktion</b>.<br><br>Ger <i>enorm förstörelse</i>."
+  },
+  {
+    front: "Fusionsenergi",
+    back: "Energi som kommer från <b>fusion</b>.<br><br>Ger <i>mycket energi</i> och lite avfall."
+  },
+  {
+    front: "Repellera",
+    back: "När något <b>stöter bort</b> något annat.<br><br>Till exempel lika laddningar."
+  },
+  {
+    front: "Attrahera",
+    back: "När något <b>dras mot</b> något annat.<br><br>Till exempel olika laddningar."
+  },
+  {
+    front: "Geigermätare",
+    back: "Ett instrument som <b>mäter strålning</b>.<br><br>Den klickar när den känner av <i>radioaktivitet</i>."
+  }
+];
