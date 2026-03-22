5b821713e102f4035b7f53e72f3c5b17f9927477
wedg3 <lindgren@gmail.com>
2026-03-22 19:17:21 +0100

    Add French vocabulary list for Chez Nous 3c

---
+++ wordlists/franska3.js
@@ -0,0 +1,40 @@
+const wordlisttitle = "Franska - Chez Nous 3c";
+
+const words = [
+  { front: "Maximes fest", back: "la fête chez Maxime" },
+  { front: "(de) kommer", back: "(elles) arrivent" },
+  { front: "till", back: "chez" },
+  { front: "nejmen...", back: "tiens" },
+  { front: "så kul", back: "génial" },
+  { front: "stig på! kom in!", back: "entrez !" },
+  { front: "så fint du har det", back: "c’est chouette<br><br>chez toi" },
+  { front: "får jag presentera...", back: "je vous présente..." },
+  { front: "godkväll", back: "bonsoir" },
+  { front: "trevligt att träffas", back: "enchantée" },
+  { front: "faktiskt", back: "en fait" },
+  { front: "jag känner inte", back: "je ne connais pas" },
+  { front: "jaså?", back: "ah, bon ?" },
+
+  { front: "ett varuhus", back: "un grand magasin" },
+  { front: "jag hittar", back: "je trouve" },
+  { front: "jag får panik", back: "je panique" },
+  { front: "men som genom ett under", back: "mais miracle" },
+
+  { front: "sedan", back: "ensuite" },
+  { front: "bjuder han mig på en coca cola", back: "il m’invite à<br><br>prendre un coca" },
+  { front: "på café", back: "au café" },
+  { front: "vi talar om allt", back: "nous discutons<br><br>de tout" },
+  { front: "filmer", back: "films" },
+  { front: "musik", back: "musique" },
+  { front: "verkligen", back: "vraiment" },
+  { front: "han vill", back: "il veut" },
+  { front: "betala", back: "payer" },
+  { front: "tom", back: "vide" },
+  { front: "sant", back: "vrai" },
+  { front: "omöjligt", back: "impossible" },
+
+  { front: "varsågod", back: "voilà" },
+  { front: "tack ännu en gång", back: "merci encore" },
+  { front: "en röd ros", back: "une rose rouge" },
+  { front: "alltför snällt", back: "trop gentil" }
+];
